a65464056db19363d83f8ed77e724cef7c5de336
sumitbhatia1986 <48047329+sumitbhatia1986@users.noreply.github.com>
2023-06-25 20:13:43 -0500

    Add files via upload

---
+++ Accept_minion.py
@@ -0,0 +1,24 @@
+import sse_api
+
+user='sample_vmwareuser'
+password='XXXXXXXX'
+sse='saltstack.dev.com'
+minion='terraform-test'
+master='Master1'
+ 
+ 
+client=sse_api.connect_api(sse,user=user,password=password,config='internal',timeout=120)
+print(client)
+
+# client should be <Response [200]>
+if client.status_code != 200:
+    print('connection failed')
+    exit
+result=sse_api.accept_key(client,minion,master)
+print(result)
+#
+#  good result
+# {'riq': 280033949565225, 'ret': '20200708144749932103', 'error': None, 'warnings': [], 'info': 'Accepted key for minion gcp6387temp10 on master gcp6387devsltmst01'}
+#
+# note this does not check if key actually exists, you just get confirmation your call was passed to master
+#
