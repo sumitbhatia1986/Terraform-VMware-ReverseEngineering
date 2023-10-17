18501418b722529615b14c5511ce003c9d7b367c
Sumit Bhatia <48047329+sumitbhatia1986@users.noreply.github.com>
2023-10-17 18:57:23 -0500

    Update psscript.ps1

--- psscript.ps1
+++ psscript.ps1
@@ -1,8 +1,8 @@
-#Sample master IP here is 136.252.235.185
-#Repository IP where the addminion.ps1 script reside is 10.196.184.8
+#Sample master IP here is x.x.x.x
+#Repository IP where the addminion.ps1 script reside is x.x.x.x
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 ; [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true } 
 $webclient = New-Object system.net.webclient
 for ($i;$i -lt 6;$i++) {
-    $webclient.DownloadFile("https://10.196.184.8:8443/repository/raw-salt-nobrowse/salt/client/addminion.ps1", "$env:TEMP\addminion.ps1") 
+    $webclient.DownloadFile("https://x.x.x.x:8443/repository/raw-salt-nobrowse/salt/client/addminion.ps1", "$env:TEMP\addminion.ps1") 
     if (Test-Path -Path $env:TEMP\addminion.ps1){break } else { sleep 5 }  }
-& "$env:TEMP\addminion.ps1" -master 136.252.235.185 -stateful -defaults
+& "$env:TEMP\addminion.ps1" -master x.x.x.x -stateful -defaults
