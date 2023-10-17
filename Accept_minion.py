fd065d1668dde08d45b99f109fff09c6c1da31c3
Sumit Bhatia <48047329+sumitbhatia1986@users.noreply.github.com>
2023-10-17 18:59:38 -0500

    Update Accept_minion.py

--- Accept_minion.py
+++ Accept_minion.py
@@ -18,7 +18,7 @@ result=sse_api.accept_key(client,minion,master)
 print(result)
 #
 #  good result
-# {'riq': 280033949565225, 'ret': '20200708144749932103', 'error': None, 'warnings': [], 'info': 'Accepted key for minion gcp6387temp10 on master gcp6387devsltmst01'}
+# {'riq': 280033949565225, 'ret': '20200708144749932103', 'error': None, 'warnings': [], 'info': 'Accepted key for minion xxxxxxx on master xxxxxxx'}
 #
 # note this does not check if key actually exists, you just get confirmation your call was passed to master
 #
